133d87e1d19d7dbbc82fdf911efa1ee6d5859ea1
NathanAlbuquerque <nathan.nra@outlook.com>
2022-04-15 19:22:43 -0300

    Passada a requisição da Rota ao Controlador

---
+++ app/Http/Controllers/CarrosselController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CarrosselController extends Controller
+{
+    public function principal()
+    {
+        return ('oi');
+    }
+}
